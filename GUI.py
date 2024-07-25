c54ca3be3d8b00212f2c88204c332edf1fbf10b3
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-24 23:32:41 -0700

    Adds buttons and creates database

--- GUI.py
+++ GUI.py
@@ -9,6 +9,7 @@ from tkinter import messagebox
 
 # Import the other 2 module scripts created for this phonebook app:
 import main
+import functionsFile
 
 
 
@@ -59,8 +60,30 @@ def load_gui(self):
     self.listBox = Listbox(self.master, exportselection=0, yscrollcommand=self.scrollbar.set)
     self.listBox.grid(row=1, rowspan=7, column=2, columnspan=3, padx=(0,0), pady=(0,0), sticky=N+E+W+S)
 
+    # Create event listener for if a user chooses a name from the list. If they do, then call the "onSelect" function from functionFile
     self.listBox.bind('<<ListboxSelect>>', lambda event: functionsFile.onSelect(self, event))
     self.scrollbar.config(command=self.listBox.yview)
+
+
+    # **************** BUTTONS *****************************
+    self.button_add = tk.Button(self.master, width=12, height=2, text='Add', command=lambda: functionsFile.addToList(self))
+    self.button_add.grid(row=8, column=0, padx=(25,0), pady=(45,10), sticky=W)
+
+    self.button_update = tk.Button(self.master, width=12, height=2, text='Update', command=lambda: functionsFile.onUpdate(self))
+    self.button_update.grid(row=8, column=1, padx=(15,0), pady=(45,10), sticky=W)
+
+    self.button_delete = tk.Button(self.master, width=12, height=2, text='Delete', command=lambda: functionsFile.onDelete(self))
+    self.button_delete.grid(row=8, column=2, padx=(15,0), pady=(45,10), sticky=W)
+
+    self.button_close = tk.Button(self.master, width=12, height=2, text='Close', command=lambda: functionsFile.ask_quit(self))
+    self.button_close.grid(row=8, column=4, padx=(15,0), pady=(45,10), sticky=E)
+
+
+    # **************** CREATE THE DATABASE *********************
+    functionsFile.create_db(self)
+
+    functionsFile.onRefresh(self)
+
 
 
 # If an attempt is made to accidentally run this GUI file as its own stand-alone module, make certain nothing occurs: 
